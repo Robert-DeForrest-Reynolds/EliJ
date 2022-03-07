0da231927efe2d0c7b7ea794a721e4bdbeda4ca7
Robert-DeForrest-Reynolds <88917345+Robert-DeForrest-Reynolds@users.noreply.github.com>
2022-03-06 20:23:59 -0800

    Large Development Update

--- Pineapple/Hope.py
+++ Pineapple/Hope.py
@@ -1,1 +1,27 @@
 # Placeholder file for Hope build system
+
+from sys import argv
+
+# Analyze given commands
+def CommandParsing():
+    pass
+
+# Project Generation
+def ProjectGeneration():
+    pass
+
+# Project Prototyping
+def ProjectPrototyping():
+    pass
+
+# Project Build
+def ProjectBuild():
+    pass
+
+# Project Run
+def ProjectRun():
+    pass
+
+# Project Clean
+def ProjectClean():
+    pass
---
+++ Pineapple/Tools/JSON_Parsing.py
@@ -0,0 +1,1 @@
+# Placeholder file for the JSON parsing module
