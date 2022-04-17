6d5516c44e19306c90b831efe003041a4c79878a
Robert-DeForrest-Reynolds <rldr.europa@gmail.com>
2022-04-17 15:54:08 -0700

    Small Update, Coming Back Later

--- Hope/Hope.py
+++ Hope/Hope.py
@@ -1,18 +1,45 @@
 # Placeholder file for Hope build system
 
 from sys import argv
+from os import mkdir
+
+# Command Lists
+New_Project_Com_List = [
+    "New",
+    "new",
+    "n"
+]
+
+# Debugging
+print("Arguments:" + str(argv) + '\n')
+
+# Project Generation Function
+def Create():
+
+    Project_Name = str(argv[2])
+
+    print("Project Name:" + Project_Name)
+    mkdir(argv[2])
+
+    with open(Project_Name + "\\" + '.gitignore', 'w+') as IGNORE_FILE:
+        IGNORE_FILE.write(".Hope_Settings" + '\n' + ".Common")
+
+    with open(Project_Name + "\\" + '.Hope_Settings', 'w+') as SETTINGS_FILE:
+        SETTINGS_FILE.write("""# Hope Project Manager Settings
+
+Iwa_Path=
+Project_Type=
+""")
 
 # Analyze given commands
 def CommandParsing():
-    # hope <primary command> <primary parameters> <secondary parameters> <tertiary-parameters>
-    pass
+    Command_Given = str(argv[1])
 
-# Project Generation
-def Create():
-    # Generate project directory
-    # Generate project template files
-    # Generate project settings file
-    pass
+    if Command_Given in New_Project_Com_List:
+        try:
+            Create()
+        except FileExistsError:
+            print("Project with that name already exists, so Hope failed to create the project.")
 
 # Project Prototype
 def Prototype():
@@ -33,3 +60,6 @@ def ReleaseBuild():
 # Project Clean
 def Clean():
     pass
+
+if __name__ == '__main__':
+    CommandParsing()
