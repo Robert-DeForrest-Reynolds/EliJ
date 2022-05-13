4933bbcccf2ecafa6fa6b6c0edc6365ffca489d7
Robert-DeForrest-Reynolds <rldr.europa@gmail.com>
2022-05-12 23:02:33 -0700

    Fixes

---
+++ Building/x86_64/Tests/x86_64-Setup_Affirm.bat
@@ -0,0 +1,2 @@
+ECHO "Checking for GCC by Running Version Check"
+START /B /WAIT msys64\mingw64 gcc Building\x86_64\Tests\x86_64_Affirm.c -o Building\x86_64\Tests\x86_64_Affirm.exe
--- Building/x86_64/Tests/x86_64-Setup_Test_1.bat
+++
@@ -1,4 +0,0 @@
-ECHO "Checking for GCC by Running Version Check"
-START /B /WAIT msys64\mingw64 gcc Building\x86_64\Tests\x86_64_Test.c -o Building\x86_64\Tests\x86_64_Test.exe
-START /B /WAIT msys64\mingw64 Building\x86_64\Tests\x86_64_Test
-Building\x86_64\Tests\x86_64_Test
