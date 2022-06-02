a23a8912f67741bf4c2288178a7bdebd4e363b95
Robert-DeForrest-Reynolds <rldr.europa@gmail.com>
2022-06-02 12:15:12 -0700

    Small update

--- Iwa_Utilities/C_Language.py
+++ Iwa_Utilities/C_Language.py
@@ -1,2 +1,5 @@
-C_IO_Import = "#include <stdio.h>"
-C_Main_Function_Opening = "int main(){"
+with open("Iwa_Utilities\C_Language.txt", 'r') as File:
+    C_Lanuage = File.readlines()
+    Include = C_Lanuage[0].strip()
+    Define = C_Lanuage[1].strip()
+    C_IO_Import = C_Lanuage[2].strip()
